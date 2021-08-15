98fdb68c230a5c2aad6b50515c1bb6df156d04a6
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-08-16 00:52:11 +0800

    added bobby logic

---
+++ README.md
@@ -0,0 +1,312 @@
+# FOUNDERS LAIR Express Web Service
+
+Create [Express] APIs to serve [founderslair] Web Application.
+
+- [Usage](#usage)
+  - [Start](#Start)
+  - [Environment](#environment)
+  - [Test](#test)
+  - [Error Handling](#error-hanfling)
+  - [API Documentation](#api-documentation)
+  - [Options](#options)
+  - [Standart Response](#standart-response)
+  - [Examples](#examples)
+  - [DB structure](#db-structure)
+- [Middlewares](#middlewares)
+- [Contributing](#contributing)
+- [Contact](#contact)
+- [Licence](#licence)
+
+## Usage
+
+### Start
+
+First you need to clone git repository:
+
+```sh
+git clone git@gitlab.com:founders-lair/express.git
+```
+Than you need to install NPM packages:
+
+```sh
+npm install
+```
+
+To start the server:
+
+```sh
+npm start
+```
+### Environment
+```sh
+    . Development
+    . Test
+    . Production
+```
+### Test
+We have automated tests for all Routes
+
+```sh
+npm test
+```
+```JS
+process.env.NODE_ENV = 'test';
+
+require('mongoose');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(require('chai-date-string'));
+
+const should = chai.should();
+
+const server = require('../app');
+const { seed, cleanup } = require('./utils');
+
+
+chai.use(chaiHttp);
+describe('Description', () => {
+  beforeEach(async () => {
+    await cleanup();
+    await seed();
+  });
+  afterEach(async () => {
+    await cleanup();
+  });
+  it('Test request ', (done) => {
+      chai.request(server)
+      .post('/register')
+      .send({ email: 'email@test.com', password: 'secret' })
+      .end(async (_err, res) => {
+        //   check types of res.body
+        //   check values of res.body
+          done();
+      });
+  });
+});
+
+```
+Terminal example
+```sh
+    /GET Projects
+      ✔ naked request should return an array of projects with basic fields
+      ✔ Get by ID request should return Project object
+      ✔ authorization should ignore application populate as its included in BlackList
+      ✔ Patch request Unauthorized
+      ✔ landing page request should return an array with the right fields for unlogged users
+    34 passing (22s)
+```
+
+### Error Handling
+In our Server we user centralized Error Handling
+```js
+const asyncHandler = require('express-async-handler');
+```
+```js
+const controller = asyncHandler(async (req, res) => {
+    // Your code
+    const condition = 'any';
+    const statusCode = 'any'
+
+  if (condition) {
+      res.status(stausCode);
+      throw new Error('Your Eroor message');
+  }
+});
+```
+#### Status Codes for Errors
+```js
+  . 400 Bad Request
+  . 401 Unauthorized
+  . 404 Not Found
+  . 500 Server Error
+```
+### API Documentation
+
+Flair Server generates API Documentation with Swagger
+
+```js
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+```
+### To Get the API Documentation
+
+```sh
+npm start
+```
+*Note API Documentation is not available on Production mode
+
+*When the serever is up and running
+
+** Open your Browser and visit http://localhost:3003/api-docs/
+
+![Image of API Documentation](https://i.imgur.com/GfURuRd.png)
+### `expressWebService( [options] )`
+
+Create and return a middleware for serving webservice endpoints, customisable.
+### Examples
+
+API creation example:
+
+In app.js file
+```JS
+const customRouter = require('./routes/V2/customRouter');
+const app = express()
+
+
+// mount on a sub-path
+app.use("/example", customRouter);
+
+```
+In router
+```JS
+const express = require('express');
+const { customFindAll, customGetById } = require('../../controllers/custom');
+const { findQuery, getQuery } = require('../../middlewares/parseQuery');
+const Model = require('../../models/model')
+
+const router = express.Router();
+
+router.get('/', findQuery(Model) customFindAll);
+router.get('/:id', getQuery(Model) customGetById);
+
+module.exports = router;
+```
+In controller
+```JS
+const customFindAll = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    ...req.Bobby
+  });
+});
+
+const customGetById = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    ...req.Bobby,
+  });
+});
+
+module.exports = { customFindAll, customGetById };
+```
+More details on how [`parseQuery`](#middlewares) workes
+What is [`Bobby`](#middlewares)
+
+The result will be
+```JS
+    {
+    "status": true,
+    "count": 10,
+    "pagination": {
+        "next": {
+            "page": 3,
+            "limit": 10
+        },
+        "prev": {
+            "page": 1,
+            "limit": 10
+        }
+    },
+    "data": [
+        // your data
+     ]
+    }
+```
+#### Standart Response
+All responses contain STATUS and DATA
+```js
+{
+    status: true,
+    data: {
+        // your data
+    }
+}
+// Note data can be Array or Object depends on what route you hit
+
+```
+Some responses contain more
+```js
+{
+    status: true,
+    count: 10,
+    pagination: {
+        next: {
+            page: 3,
+            limit: 10
+        },
+        prev: {
+            page: 1,
+            limit: 10
+        }
+    },
+    data: [
+        // your data
+     ]
+    }
+// Note data can be Array or Object depends on what route you hit
+
+```
+### Middlewares
+
+## ParseQuery
+ This is a middleware which takes request Query, Model
+
+ It gives client ability to populate, select, paginate, limit, order and make basic query with the req.query
+ example
+ https://example_page.to/exaples?select=_id+user&page=2&limit=3&populate=user&limit=3
+```JS
+    req.Bobby ={
+        "status": true,
+        "count": 3,
+        "pagination": {
+          "next": {
+             "page": 3,
+              "limit": 3
+          },
+          "prev": {
+               "page": 1,
+               "limit": 3
+           }
+         },
+    "data": [
+        {
+            "_id": "6112b1ac8954580004ceae52",
+            "user":{"users populated data here"}
+        }
+    ]
+```
+
+### DB structure
+
+![Image of DB structure](https://i.imgur.com/OPQ0t1R.png)
+#### About (`/__about`)
+
+[Founders Lair][founderslair], or FLair, is a free peer to peer platform for founders & startup enthusiasts, which strives to help entrepreneurs find faster the resources they need. Founders Lair is like a secret platform, a hideout that you can only enter and contribute to if you are a part of the startup ecosystem.
+
+Anybody traveling to another startup ecosystem, anyone interested in starting their business or navigating in their own city relies on insights of who they can trust. Having access to reliable information about startup ecosystems is however something most do not have. Finding trustworthy partners is a time intensive and tiring process. This is why we started Founders Lair.
+
+#### Error (`/__error`)
+
+This endpoint simply throws a JavaScript error, and makes no attempt to handle it. This is useful for testing the way that your application handles runtime errors in prod, both in terms of reporting them (eg to a logging or aggregation tool) and presenting an appropriate UI to the end user.
+
+## Contributing
+
+This module has a full suite of unit tests, and is verified with ESLint. You can use the following commands to check your code before opening a pull request.
+
+```sh
+npm test    # run the unit tests and check coverage
+```
+
+## Contact
+
+If you have any questions or comments about this module, or need help using it, please visit [#flair-support] or email [Flair Back End Support].
+
+## Licence
+
+This software is published by the Founders Lair under the [MIT licence][license].
+
+[#flair-support]: https://founderslair.com/about
+[express]: http://expressjs.com/
+[license]: http://opensource.org/licenses/MIT
+[node.js]: https://nodejs.org/
+[npm]: https://www.npmjs.com/
+[Flair Back End Support]: jon@founderslair.com
+[founderslair]: https://founderslair.com/
---
+++ index.js
@@ -0,0 +1,318 @@
+const asyncHandler = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+
+const helper = (perm, query, next) => {
+  let newReq = query;
+  // checking if query has select
+  if (perm.select && query.select) {
+    const filtered = [];
+    // if has looping trough to se do we have match with restricted fields
+    query.select.split(' ').forEach((i) => {
+      if (!perm.select.includes(i)) {
+        filtered.push(i)
+      }
+    });
+    // than resetting filtered select field
+    query.select = filtered.join(' ');
+  }
+  // checking if no select and we have restrictions, we need to remove some fields by setting select
+  if (perm.select && !query.select) {
+    const filtered = [];
+    perm.select.split(' ').forEach((i) => {
+      filtered.push(`-${i}`)
+    });
+    query.select = filtered.join(' ');
+  }
+  // checking for restrictions on populate
+  if (perm.populate.length && query.populate) {
+    const filtered = [];
+    // checking if populate field in query is an Array
+    if (Array.isArray(query.populate)) {
+      query.populate = query.populate.map((item) => {
+        if (typeof item === 'object') {
+          if (!perm.populate.includes(item.path)) {
+            if (perm.dontSelect) {
+              perm.dontSelect.forEach((dont) => {
+                if (dont.path === item.path) {
+                  if (item.select) {
+                    item.select = item.select.split(' ').map((i) => {
+                      if (dont.select.includes(i)) {
+                        return '';
+                      }
+                      return i;
+                    }).join(' ');
+                  }
+                  if (!item.select) {
+                    item.select = dont.select.map(i => `-${i}`);
+                  }
+                // ensuring user cant go dipper than 2 layers of nested population
+                  if (item.populate && item.populate.populate) {
+                    // eslint-disable-next-line no-param-reassign
+                    item.populate.populate = '';
+                  }
+                }
+              });
+            }
+            filtered.push(item);
+          }
+          return ''
+        }
+        if (typeof item === 'string' && perm.dontSelect) {
+            // check if item is matching with perm dontSelect path
+            if (perm.populate) {
+                if (perm.populate.includes(item)) {
+                    item = ''
+                  }  
+            } 
+            perm.dontSelect.forEach((dont) => {
+                if (dont.path === item) {
+                    item = ({path: item, select:dont.select.map(i => `-${i}`)});
+                }
+            });
+            
+        }
+        if (!perm.populate.includes(item)) {
+        
+          filtered.push(item);
+        }
+        return ''
+      });
+      newReq.populate = filtered
+    }
+    if (typeof query.populate === 'string') {
+    //   query.populate = query.populate.split(' ').filter((field) => !perm.populate.includes(field)).join(' ');
+      query.populate = '';
+      return next();
+    }
+    if (typeof query.populate === 'object' && !Array.isArray(query.populate)) {
+      // perm.populate.forEach( (restricted) => {
+      //   if (query.populate.path === restricted) {
+      //     query.populate = '';
+      //   }
+      // })
+      query.populate = '';
+      return next();
+    }
+  }
+  return next();
+};
+
+const findQuery = (model, populate) =>
+  asyncHandler(async (req, res, next) => {
+    let query;
+    // req.query.deleted = false;
+    // Copy req.query
+    const reqQuery = { ...req.query };
+    // Fields to exclude from filtering
+    const removeFields = ['select', 'sort', 'limit', 'page', 'populate'];
+    // loop over removeFields and delet them from reqQuery
+    removeFields.forEach((item) => delete reqQuery[item]);
+    // Create Query string
+    let queryStr = JSON.stringify(reqQuery);
+
+    queryStr = queryStr.replace(/\b(or|gte)\b/g, (match) => `$${match}`);
+    // Finding resources
+    query = model.find(JSON.parse(queryStr));
+    // Finding resources
+
+    // Select fields
+    if (req.query.select) {
+      const fields = req.query.select;
+      query = query.select(fields);
+    }
+    // Sort fields
+    if (req.query.sort) {
+      const sortBy = req.query.sort;
+      query = query.sort(sortBy);
+    }
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await model.countDocuments();
+    query.skip(startIndex).limit(limit);
+    if (populate) {
+      query = query.populate(populate);
+    }
+    if (req.query.populate) {
+      query = query.populate(req.query.populate);
+    }
+    // Executing query
+    const items = await query;
+    // Pagination result
+    const pagination = {};
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
+    }
+    const data = {
+      status: true,
+      count: items.length,
+      pagination,
+      data: items,
+    };
+    if (req.extraData) {
+      req.Bobby = { ...req.extraData, ...data };
+    } else {
+      req.Bobby = data;
+    }
+
+    return next();
+  });
+
+const getQuery = (model, populate) =>
+  asyncHandler(async (req, res, next) => {
+    let query;
+
+    if (req.params.id) {
+      query = model.findById(req.params.id);
+    }
+    if (req.params.slug) {
+      query = model.findOne({ slug: req.params.slug });
+    }
+    // Select fields
+    if (req.query.populate) {
+      const fields = req.query.populate;
+      query = query.populate(fields);
+    }
+    if (populate) {
+      query = query.populate(populate);
+    }
+    if (req.body.populate) {
+      query = query.populate(req.body.populate);
+    }
+    if (req.query.select) {
+      const fields = req.query.select;
+      query = query.select(fields);
+    }
+    // Executing query
+    const item = await query;
+    if (!item) {
+      res.status(404);
+      throw new Error(`Couldn't find the item with an id : ${req.params.id}`);
+    }
+    req.Bobby = {
+      status: true,
+      data: item,
+    };
+    next();
+  });
+
+
+
+
+const CRUD = (Model) => {
+    const find = asyncHandler(async (req, res) => {
+        res.status(200).json({
+            ...req.Bobby
+        });
+    });
+    const get = asyncHandler(async (req, res) => {
+        res.status(200).json({
+            ...req.Bobby
+        });
+    });
+    const patch = asyncHandler(async (req, res) => {
+        const { id } = req.params
+        const item = await Model.findById(id);
+        if (!item) {
+            res.status(400);
+            throw new Error(`[Bobby] Cant Get item with an ID: ${id}`);
+        }
+        const updatedItem = await Model.findByIdAndUpdate(id, req.body, {new: true});
+        if (!updatedItem) {
+            res.status(400);
+            throw new Error(`[Bobby] Cant Update item with an ID: ${id}`);
+        }
+        res.status(200).json({
+            status: true,
+            data: updatedItem,
+        });
+    });
+    const create = asyncHandler(async (req, res) => {
+        const item = await Model.create(req.body);
+        if (!item) {
+            res.status(400);
+            throw new Error('[Bobby] Cant Create item');           
+        }
+        res.status(200).json({
+            status: true,
+            data: item,
+        });
+    });
+    const remove = asyncHandler(async (req, res) => {
+        const { id } = req.params
+        const item = await Model.findById(id);
+        if (!item) {
+            res.status(400);
+            throw new Error(`[Bobby] Cant Get item with an ID: ${id}`);
+        }
+        const deleteItem = await Model.findByIdAndRemove(id);
+        if (!deleteItem) {
+            res.status(400);
+            throw new Error(`[Bobby] Cant Update item with an ID: ${id}`);
+        }
+        res.status(204);
+    });
+    return {
+        find,
+        get,
+        patch,
+        create,
+        remove,
+    };
+};
+
+const clearQuery = (fields) => (req, res, next) => {
+    const reqQuery = { ...req.query };
+    fields.forEach((item) => delete reqQuery[item]);
+    req.query = reqQuery;
+    return next();
+  };
+  
+const authorize = (permissions) => asyncHandler(async (req, res, next) => {
+    let isObject = false;
+    if (req.query.populate && (req.query.populate.includes('}') || req.query.populate.includes(']'))) {
+        isObject = true;
+    }
+    if (isObject && JSON.parse(req.query.populate)) {
+        req.query.populate = JSON.parse(req.query.populate);
+    }
+  
+        helper(permissions, req.query, next);
+        return;
+});
+
+const handleErrors = {
+    notFound: (req, res, next) => {
+        const error = new Error(`Not found - ${req.originalUrl}`);
+        res.status(404);
+        next(error);
+      },
+    errorHandler : (err, req, res, next) => {
+        const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+        res.status(statusCode);
+        res.json({
+          message: err.message,
+          stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+        });
+        next();
+      }
+}
+
+module.exports = { 
+    authorize,
+    getQuery,
+    findQuery,
+    clearQuery,
+    CRUD,
+    handleErrors,
+};
